50fc8c83a0478b23175259930554a51e4b9f82ae
Samuel Hale <sjhale@my.waketech.edu>
2021-03-21 04:15:46 -0400

    this is pretty cool good night

--- public/scripts/searchbar.js
+++ public/scripts/searchbar.js
@@ -9,11 +9,41 @@ document.addEventListener('DOMContentLoaded', function() {
     //search api for food and filter it
     const searchFood = async searchText => {
 
-    const res = await fetch('https://api.spoonacular.com/food/search?apiKey=5e89818d38f44b53b8c1391fd53cbb6d&includeNutrition=true&query='+ search.value);
-    const foods = await res.json();
+        const res = await fetch('https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=5e89818d38f44b53b8c1391fd53cbb6d&includeNutrition=true&metaInformation=true&query='+ search.value);
+        const foods = await res.json();
+        
+        var matches = foods.filter(food =>{
 
-    console.log(foods.searchResults[5].results);
+            const regex = new RegExp(`^${searchText}`,'gi');
+            return food.name.match(regex);
 
+        });
+
+        if(searchText.length === 0){
+            matches = [];
+            matchList.innerHTML = '';
+        }
+        
+        outputHtml(matches);
+
+    };
+
+
+
+    const outputHtml = matches =>{
+        if(matches.length > 0){
+            const html = matches
+            .map(match => `
+                <div>
+                    <h4>${match.name} <img src="https://spoonacular.com/cdn/ingredients_100x100/${match.image}"></h4>
+                </div>
+
+            `
+             )
+             .join('');
+
+            matchList.innerHTML = html;
+        }
     };
 
 
