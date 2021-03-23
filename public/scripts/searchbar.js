2d09dadd2f6f10288d06006b3be11e88020f73c7
Samuel Hale <sjhale@my.waketech.edu>
2021-03-22 23:45:34 -0400

    should work when api limits are gone

--- public/scripts/searchbar.js
+++ public/scripts/searchbar.js
@@ -1,4 +1,12 @@
 
+/*
+    apikey1 = 5e89818d38f44b53b8c1391fd53cbb6d
+    apikey2 = 8c0c7132fb5a4a12901cc6ba13001aaf
+
+
+*/
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
     const search = document.getElementById('foodinput');
@@ -10,45 +18,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchFood = async searchText => {
         
     
-        const res1 = await fetch('https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=5e89818d38f44b53b8c1391fd53cbb6d&includeNutrition=true&metaInformation=true&number=4&query='+ search.value);
-        const res2 = await fetch('https://api.spoonacular.com/food/menuItems/search?apiKey=5e89818d38f44b53b8c1391fd53cbb6d&includeNutrition=true&number=4&query='+ search.value);
+        const res1 = await fetch('https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=5e89818d38f44b53b8c1391fd53cbb6d&includeNutrition=true&metaInformation=true&number=3&query='+ search.value);
+        const res2 = await fetch('https://api.spoonacular.com/food/products/search?apiKey=8c0c7132fb5a4a12901cc6ba13001aaf&includeNutrition=true&number=3&query='+ search.value);
         
         var ingredients = await res1.json();
-        var menuItems = await res2.json();
-
-        menuItems.menuItems = menuItems.menuItems.map(function(obj) { 
-            obj['name'] = obj['title']; // Assign new key 
-            delete obj['title']; // Delete old key 
-            return obj; 
-        }); 
+        var products = await res2.json();
+        
+        
         
 
-        var foodlist = ingredients.concat(menuItems.menuItems);
-        
-        console.log(foodlist);
-       
+        ingredients = ingredients.filter(food => {return food});
+        products = products.products.filter(food => {return food});
 
-        var matches = foodlist.filter(food =>{
-
-            return food;
-
-        }); 
 
         if(searchText.length === 0){
-            matches = [];
+            ingredients = [];
+            products = [];
             matchList.innerHTML = '';
         }
         
-        outputHtml(matches);
+        outputHTML(ingredients,products);
+
 
     };
 
 
 
-    const outputHtml = matches =>{
+    function outputHTML(ingredients,products){
 
-        if(matches.length > 0){
-            const html = matches
+        if(ingredients.length > 0 && products.length > 0){
+            var html1 = ingredients
             .map(match => `
                 <div class="card" >
                     <h4>${match.name} <img src="https://spoonacular.com/cdn/ingredients_100x100/${match.image}"></h4>
@@ -57,11 +56,27 @@ document.addEventListener('DOMContentLoaded', function() {
             `
             ).join('');
 
-            matchList.innerHTML = html;
+
+            
+            var html2 = products
+            .map(match => `
+                <div class="card" >
+                    <h4>${match.title} <img src="${match.image}"></h4>
+                </div>
+    
+            `
+            ).join('');
+    
+            
+
+            matchList.innerHTML = html1 + html2;
+
         }
 
     };
 
+
+    
 
 
     search.addEventListener('input', () => searchFood(search.value) ); 
