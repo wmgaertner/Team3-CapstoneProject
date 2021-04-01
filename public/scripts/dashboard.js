3800d894b73f729ecb1697f32e977e6e9d369a61
Samuel Hale <sjhale@my.waketech.edu>
2021-04-01 16:16:25 -0400

    perma fix for am pm

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -121,10 +121,10 @@ function clock(){
     var today = new Date();
     var hours = today.getHours()
     var minutes = today.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours != 0 ? hours : 12;
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    var ampm = hours >= 12 ? 'PM' : 'AM';
     timestamp = 'Time: ' + hours.toString() + ':' + minutes.toString() + ' ' + ampm;
 
 
--- public/scripts/timestamps.js
+++ public/scripts/timestamps.js
@@ -3,10 +3,10 @@
 function maketimestamp(now){
     var hours = now.getHours()
     var minutes = now.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours != 0 ? hours : 12;
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    var ampm = hours >= 12 ? 'PM' : 'AM';
     timestamp = hours.toString() + ':' + minutes.toString() + ' ' + ampm;
     return timestamp;
 }
